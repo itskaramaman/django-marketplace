7f507b967b92aeaba5628d07ac4bca4348c3c6d6
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-08-10 13:36:58 -0700

    conversations added

--- puddle/urls.py
+++ puddle/urls.py
@@ -8,6 +8,7 @@ from core.views import index, contact
 urlpatterns = [
     path('', include('core.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('conversation/', include('conversation.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
